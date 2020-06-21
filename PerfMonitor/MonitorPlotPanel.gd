a1e30a73e118f68d63eba7b67fa41028a2123c60
Aleksandr Bazhin <aleksandrbazhin@gmail.com>
2020-06-22 01:10:41 +1000

    removed buffer array

--- PerfMonitor/MonitorPlotPanel.gd
+++ PerfMonitor/MonitorPlotPanel.gd
@@ -7,9 +7,6 @@ const DEFAULT_LEN: int = 180
 const DEFAULT_COLOR: Color = Color(0.2, 1, 0.2, 0.5)
 const BG_COLOR: Color = Color(0.27, 0.32, 0.35, 0.91)
 
-
-var plot_data_int: PoolIntArray = []
-var plot_data_float: PoolRealArray = []
 var plot_len: int = DEFAULT_LEN
 var plot_pointer: int = 0
 var plot_last_data: float = 0
@@ -27,14 +24,12 @@ var plot_image: Image
 var plot_image_texture: ImageTexture
 var plot_image_blit_rect: Rect2 = Rect2(Vector2.ZERO, Vector2.ZERO)
 
-
 onready var label_node: Label = $Label
-onready var plot_data_array = plot_data_int
 
 
 func init_plot(label: String, data_max: float, plot_length_frames: int = DEFAULT_LEN, 
 		color: Color = DEFAULT_COLOR, size: Vector2 = DEFAULT_SIZE, 
-		is_data_int: bool = true, is_humanise_needed: bool = false):
+		_is_data_int: bool = true, is_humanise_needed: bool = false):
 	plot_len = plot_length_frames
 	data_label = label
 	graph_size = size
@@ -43,12 +38,7 @@ func init_plot(label: String, data_max: float, plot_length_frames: int = DEFAULT
 	plot_color = color
 	is_mem_size = is_humanise_needed
 	reset_max_data(data_max)
-	if is_data_int:
-		plot_data_array = plot_data_int
-	else:
-		plot_data_array = plot_data_float
-	init_data_array()
-	
+
 	plot_image = Image.new()
 	plot_image.create(int(rect_min_size.x), int(rect_min_size.y), false, Image.FORMAT_RGBA8)
 	plot_image_texture = ImageTexture.new()
@@ -77,14 +67,10 @@ func update_scale():
 	data_scale = graph_size.y / float(perf_data_max)
 	range_scale = graph_size.x / float(plot_len)
 	
-	
-func init_data_array():
-	plot_data_array.resize(plot_len)
-	for i in range(plot_len):
-		plot_data_array[i] = 0
 
 func get_data():
 	return 0.0
+
 
 func _process(_delta):
 	resize_height()
@@ -92,9 +78,6 @@ func _process(_delta):
 	if plot_last_data > perf_data_max:
 		reset_max_data(plot_last_data)
 	label_node.text = "%s: " % data_label + get_data_str(plot_last_data)
-	if plot_data_array.size() == 0: 
-		return
-	plot_data_array[plot_pointer] = plot_last_data
 	update()
 	if plot_pointer < plot_len-1:
 		plot_pointer += 1
@@ -103,13 +86,9 @@ func _process(_delta):
 	
 
 func _draw():
-	if plot_data_array.size() == 0:
-		print("ERROR: PLOT WAS NOT SET UP ", name)
-		return
-
 	plot_image.lock()
 	plot_image.blit_rect(plot_image, plot_image_blit_rect, Vector2.ZERO)
-	var line_start = graph_size.y + plot_offset - data_scale*plot_data_array[plot_pointer - 1]
+	var line_start = graph_size.y + plot_offset - data_scale * plot_last_data
 	for i in range(graph_size.y):
 		if i > line_start:
 			plot_image.set_pixel(179, i, plot_color)
